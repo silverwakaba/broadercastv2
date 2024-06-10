e672ca41135bcc3838c116dd4c30a2461856c918
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2024-06-10 18:15:26 +0700

    Rev: Vercel php runtime version

--- routes/channels.php
+++ routes/channels.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Broadcast;
 //     return (int) $user->id === (int) $id;
 // });
 
-Broadcast::channel('baseChannel', function () {
-    return true;
-});
+// Broadcast::channel('baseChannel', function () {
+//     return true;
+// });
 
-Broadcast::channel('usersChannel', function () {
-    return true;
-});
+// Broadcast::channel('usersChannel', function () {
+//     return true;
+// });
