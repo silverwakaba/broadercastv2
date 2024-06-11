a755c5f7c20273b1f381cb003b51c955d96e5601
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2024-06-12 00:04:53 +0700

    csrf

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf" content="{{ csrf_token() }}">
         <title>@yield('title', 'Page') | {{ config('app.name', 'BASE') }}</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css">
