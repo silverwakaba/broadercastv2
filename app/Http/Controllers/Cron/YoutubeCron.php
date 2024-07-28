38196e165cae3b90fdf37154f53069e175003ff3
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2024-07-28 20:23:25 +0700

    Rev: Restore cron function

--- app/Http/Controllers/Cron/YoutubeCron.php
+++ app/Http/Controllers/Cron/YoutubeCron.php
@@ -16,62 +16,24 @@ use Illuminate\Support\Collection;
 class YoutubeCron extends Controller{
     // Debug
     public function fetchDebug(){
-        // return YoutubeRepositories::fetchVideoViaScraper("G9rODPToGr8");
-        // $api = YoutubeRepositories::fetchVideoViaScraper('Z5854fRspzo');
-
-        // return YoutubeRepositories::fetchArchiveViaFeed('UCo6mwsozDGMW0UNGowBJGhg', 2);
-
-        // return UserFeed::where([
-        //     ['base_link_id', '=', 2],
-        //     ['base_status_id', '=', 6],
-        //     ['schedule', '=', null],
-        //     ['actual_start', '=', null],
-        //     ['actual_end', '=', null],
-        //     ['duration', '=', null],
-        // ])->take(50)->get();
-
-        //
-
-        //
-
-        $datas = UserFeed::where([
-            ['base_link_id', '=', 2],
-            ['base_status_id', '=', 6],
-            ['schedule', '=', null],
-            ['actual_start', '=', null],
-            ['actual_end', '=', null],
-            ['duration', '=', null],
-        ])->select('identifier')->take(50)->get();
-
-        if(($datas) && isset($datas) && ($datas->count() >= 1)){
-            $videoID = implode(',', ($datas)->pluck('identifier')->toArray());
-
-            $http = YoutubeRepositories::apiCall('video', $videoID);
-
-            if($http['pageInfo']['totalResults'] >= 1){
-                echo "Update $videoID";
-            }
-            else{
-                return UserFeed::whereIn('identifier', explode(',', $videoID))->get();
-            }
-        }
+        // Commented
     }
 
     // Archive initialization
     public function init(){
-        // // Init
-        // UserLinkTracker::where([
-        //     ['base_link_id', '=', 2],
-        //     ['initialized', '=', false],
-        // ])->select('identifier', 'users_id')->chunk(100, function(Collection $chunks){
-        //     foreach($chunks as $chunk){
-        //         try{
-        //             // Fetch acrhive via Youtube API
-        //             YoutubeRepositories::fetchArchiveViaAPI($chunk->identifier, $chunk->users_id);
-        //         }
-        //         catch(\Throwable $th){}
-        //     }
-        // });
+        // Init
+        UserLinkTracker::where([
+            ['base_link_id', '=', 2],
+            ['initialized', '=', false],
+        ])->select('identifier', 'users_id')->chunk(100, function(Collection $chunks){
+            foreach($chunks as $chunk){
+                try{
+                    // Fetch acrhive via Youtube API
+                    YoutubeRepositories::fetchArchiveViaAPI($chunk->identifier, $chunk->users_id);
+                }
+                catch(\Throwable $th){}
+            }
+        });
 
         // Metadata
         UserLinkTracker::where([
@@ -83,7 +45,7 @@ class YoutubeCron extends Controller{
                     // Fetch acrhive via Youtube Feed
                     YoutubeRepositories::fetchArchiveViaFeed($chunk->identifier, $chunk->users_id);
 
-                    // // Update archive metadata after fetched from Youtube API
+                    // Update archive metadata after fetched from Youtube API
                     YoutubeRepositories::userFeedInit();
                 }
                 catch(\Throwable $th){}
@@ -91,9 +53,8 @@ class YoutubeCron extends Controller{
         });
     }
 
-    // Checker
+    // Feed Checker
     public function checker(){
-        // Just another checker
         UserFeed::where([
             ['base_link_id', '=', 2],
             ['actual_end', '=', null],
@@ -114,7 +75,6 @@ class YoutubeCron extends Controller{
 
     // Profiler
     public function profiler(){
-        // Profile metadata
         UserLinkTracker::where([
             ['base_link_id', '=', 2],
             ['initialized', '=', true],
