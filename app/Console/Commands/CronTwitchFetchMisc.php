c481e814b3d4fb9a169eb9d30d84747fca4befc1
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2024-08-22 23:11:30 +0700

    Rev: Unidentified variable for twitch cron

--- app/Console/Commands/CronTwitchFetchMisc.php
+++ app/Console/Commands/CronTwitchFetchMisc.php
@@ -24,7 +24,7 @@ class CronTwitchFetchMisc extends Command{
     /**
      * Execute the console command.
      */
-    public function handle(){
+    public function handle(TwitchCron $cron){
         $cron->misc();
     }
 }
--- app/Console/Commands/CronTwitchFetchProfile.php
+++ app/Console/Commands/CronTwitchFetchProfile.php
@@ -24,7 +24,7 @@ class CronTwitchFetchProfile extends Command{
     /**
      * Execute the console command.
      */
-    public function handle(){
+    public function handle(TwitchCron $cron){
         $cron->profiler();
     }
 }
