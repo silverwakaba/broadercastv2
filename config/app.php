f071d5f75c2239fec8fa01b66ef9969cac11b251
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2024-10-20 18:56:52 +0700

    Rev: HCaptcha aliases fix (2)

--- config/app.php
+++ config/app.php
@@ -151,20 +151,5 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Class Aliases
-    |--------------------------------------------------------------------------
-    |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
-    |
-    */
-
-    'aliases' => Facade::defaultAliases()->merge([
-        'HCaptcha' => \Scyllaly\HCaptcha\Facades\HCaptcha::class,
-    ])->toArray(),
-
+    
 ];
