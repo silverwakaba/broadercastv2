5f78788d48bbcd7e37966f108fc4c0f221c5e19f
silverwakaba <80201645+silverwakaba@users.noreply.github.com>
2025-02-09 11:38:53 +0700

    add link on email body

--- resources/views/layout/email.blade.php
+++ resources/views/layout/email.blade.php
@@ -10,6 +10,7 @@
         <br />
         <p>Best regards,<br />The {{ config('app.name', 'vTual') }} Team</p>
         <hr />
-        <p><small>Since we send out email notification like this in large quantities, this email may end up in your spam mailbox. You can whitelist this email address so that you can receive notifications smoothly in the future. Thank you.</small></p>
+        <p><small>Verify the authenticity and validity of the email sender by referring to the official notice regarding our official email addresses, <a href="https://help.silverspoon.me/docs/silverspoon/domain" target="_blank" rel="noopener">which you can read here</a>.</small></p>
+        <p><small>Since we send out email notification like this in large quantities, this email may end up in your spam mailbox. You can whitelist this email address so that you can receive notifications smoothly in the future.</small></p>
     </body>
 </html>
